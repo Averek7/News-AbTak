837530478432b0e1074d0b992a88de91e5b862f6
Derek <singhavi2122@gmail.com>
2021-09-16 23:45:01 +0530

    navbar commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-
 import React, { Component } from 'react'
+import Navbar from './components/Navbar.js'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        
+        <Navbar/>
       </div>
     )
   }
