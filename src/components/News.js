84baa5d42190dea187a1b9cae24fdd627c19b8da
Derek <singhavi2122@gmail.com>
2021-09-18 10:47:14 +0530

    fetch api commit

--- src/components/News.js
+++ src/components/News.js
@@ -1,27 +1,40 @@
 import React, { PureComponent } from 'react'
 import NewsItem from './NewsItem'
-import PropTypes from 'prop-types'
 
 export default class News extends PureComponent {
-    static propTypes = {
+    constructor() {
+        super();
 
+        this.state = {
+            articles: [],
+        };
+    }
+
+    componentDidMount() {
+        fetch('https://newsapi.org/v2/top-headlines?country=in&apiKey=87e58df78d7a432d9bd1621b200b95a6').then(response => {
+            return response.json();
+        }).then(result => {
+            console.log(result);
+            this.setState({articles: result.articles}); 
+        })
     }
 
     render() {
         return (
             <div>
-                <div className="row">
-                    <div className="col-md-4">
-                        <NewsItem title="myTitle" description="myDesc" imgUrl="myImg"/>
-                    </div>
-                    <div className="col-md-4">
-                        <NewsItem title="myTitle" description="myDesc" imgUrl="myImg"/>
-                    </div>
-                    <div className="col-md-4">
-                        <NewsItem title="myTitle" description="myDesc" imgUrl="myImg"/>
+                <div className="container my-3">
+                    <div className="row ">
+                        {this.state.articles.map((elem) => (
+                         <div className="col-md-4" key={elem.url}>
+                            <NewsItem title={elem.title} description={elem.description} imgUrl={elem.urlToImage} NewsUrl={elem.url} />
+                        </div>
+                        ))}
                     </div>
                 </div>
             </div>
         )
     }
+
+
 }
+
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+
     render() {
-        let {imgUrl, title, description } = this.props;
+        let {imgUrl, title, description, NewsUrl} = this.props;
         return (
             <div>
-                <div className="container my-3">
-                    <div className="card d-flex flex-row m-4" style={{width: "18rem"}}>
-                        <img src = {imgUrl} className="card-img-top" alt="..." />
+                <div className="my-3">
+                    <div className="card" style={{width: "18rem"}}>
+                        <img src = {!imgUrl ? `Image Not Found !` : imgUrl} className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">{title}</h5>
-                            <p className="card-text">{description}</p>
-                            <a href="#" class="btn btn-sm btn-primary">Know more</a>
+                            <p className="card-text">{description}...</p>
+                            <a href = {NewsUrl} className="btn btn-sm btn-primary">Know more</a>
                         </div>
                     </div>
                 </div>
